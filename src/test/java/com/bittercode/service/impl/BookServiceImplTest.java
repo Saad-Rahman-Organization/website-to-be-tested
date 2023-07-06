b04efcd823190711efb2c1f4442ff37bd2601913
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-06 02:40:11 -0400

    configered pittest

--- src/test/java/com/bittercode/service/impl/BookServiceImplTest.java
+++ src/test/java/com/bittercode/service/impl/BookServiceImplTest.java
@@ -1,7 +1,31 @@
 package com.bittercode.service.impl;
 
+import com.bittercode.model.Book;
+import com.bittercode.model.StoreException;
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Random;
 
 public class BookServiceImplTest extends TestCase {
+@Test
+public void testMutationTool() throws StoreException
+{
+    Book[] books;
+    books=new Book[20];
+    BookServiceImpl bookService = new BookServiceImpl();
+    List<Book> book=bookService.getAllBooks();
+    int i;
+    i=0;
+    for (Book b:book)
+    {
+        books[i]=b;
+        i++;
+    }
 
+    Random rand=new Random();
+    int choice= rand.nextInt(12);
+    Book b=bookService.getBookById(books[choice].getBarcode());
+}
 }
